a0cc25d76e4696ce7a3319479fd12842c499d89c
cwww3 <2239354893@qq.com>
2021-09-16 17:28:16 +0800

    consistenthash

---
+++ consistenthash/consistenthash.go
@@ -0,0 +1,60 @@
+package consistenthash
+
+import (
+	"hash/crc32"
+	"sort"
+	"strconv"
+)
+
+// Hash maps bytes to uint32
+type Hash func(data []byte) uint32
+
+// Map constains all hashed keys
+type Map struct {
+	hash     Hash
+	replicas int // 虚拟节点倍数 replicas
+	keys     []int // 存虚拟节点的哈希值
+	hashMap  map[int]string // 虚拟节点与真实节点的映射表  键是虚拟节点的哈希值，值是真实节点的名称。
+}
+
+// New creates a Map instance
+func New(replicas int, fn Hash) *Map {
+	m := &Map{
+		replicas: replicas,
+		hash:     fn,
+		hashMap:  make(map[int]string),
+	}
+	if m.hash == nil {
+		m.hash = crc32.ChecksumIEEE
+	}
+	return m
+}
+
+// Get gets the closest item in the hash to the provided key.
+func (m *Map) Get(key string) string {
+	if len(m.keys) == 0 {
+		return ""
+	}
+	hash := int(m.hash([]byte(key)))
+	// Binary search for appropriate replica.
+	// 顺时针找到第一个匹配的虚拟节点的下标 idx，从 m.keys 中获取到对应的哈希值。
+	// idx == len(m.keys) 说明没有找到符合条件了 因为是环状的 所以都分配在了idx=0的节点
+	idx := sort.Search(len(m.keys), func(i int) bool {
+		return m.keys[i] >= hash
+	})
+
+	return m.hashMap[m.keys[idx%len(m.keys)]]
+}
+
+// Add adds some keys to the hash.
+// 添加真实节点
+func (m *Map) Add(keys ...string) {
+	for _, key := range keys {
+		for i := 0; i < m.replicas; i++ {
+			hash := int(m.hash([]byte(strconv.Itoa(i) + key)))
+			m.keys = append(m.keys, hash)
+			m.hashMap[hash] = key
+		}
+	}
+	sort.Ints(m.keys)
+}
