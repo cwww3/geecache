712aebab0976d2655bdcd0a1a936417d14354348
cwww3 <2239354893@qq.com>
2021-09-18 09:41:50 +0800

    singleflight

---
+++ singleflight/singleflight.go
@@ -0,0 +1,42 @@
+package singleflight
+
+import "sync"
+
+// 代表正在进行中，或已经结束的请求。使用 sync.WaitGroup 锁避免重入。
+type call struct {
+	wg  sync.WaitGroup
+	val interface{}
+	err error
+}
+
+// 管理不同 key 的请求(call)
+type Group struct {
+	mu sync.Mutex       // protects m
+	m  map[string]*call
+}
+
+// 并发协程之间不需要消息传递，非常适合 sync.WaitGroup
+func (g *Group) Do(key string, fn func() (interface{}, error)) (interface{}, error) {
+	g.mu.Lock()
+	if g.m == nil {
+		g.m = make(map[string]*call)
+	}
+	if c, ok := g.m[key]; ok {
+		g.mu.Unlock()
+		c.wg.Wait()
+		return c.val, c.err
+	}
+	c := new(call)
+	c.wg.Add(1)
+	g.m[key] = c
+	g.mu.Unlock()
+
+	c.val, c.err = fn()
+	c.wg.Done()
+
+	g.mu.Lock()
+	delete(g.m, key)
+	g.mu.Unlock()
+
+	return c.val, c.err
+}
